6c94acb19ad41091ee74f6b0ed013ccef16ebf6d
Nigel Babu <nigelb@redhat.com>
2017-08-09 13:15:42 +0530

    Fix an error with tar for aborted regressions
    Change-Id: I1f5493eabf884bdd265e0baa11aa346d45ee2a6f

--- build-gluster-org/scripts/regression-abort.sh
+++ build-gluster-org/scripts/regression-abort.sh
@@ -6,6 +6,6 @@ UNIQUE_ID="${JOB_NAME}-${BUILD_ID}"
 SERVER=$(hostname)
 
 filename="${ARCHIVED_LOGS}/glusterfs-logs-${UNIQUE_ID}.tgz"
-sudo -E bash tar -czf "${ARCHIVE_BASE}/${filename}" /var/log/glusterfs /var/log/messages*;
+sudo -E tar -czf "${ARCHIVE_BASE}/${filename}" /var/log/glusterfs /var/log/messages*;
 echo "Logs archived in http://${SERVER}/${filename}"
 sudo reboot

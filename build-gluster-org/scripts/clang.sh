a704ed52cbeffa0d0a29829ad17feac5c2a056a3
Nigel Babu <nigelb@redhat.com>
2017-08-14 17:15:45 +0530

    Run clang in a fedora chroot
    This job builds a chroot, installs clang, copies the code inside, does
    a clang analyze inside the chroot and then copies the results out.
    
    Change-Id: I3899cf0a41c3ff5c772cb6d98b15ff1634f8cdb8

--- build-gluster-org/scripts/clang.sh
+++ build-gluster-org/scripts/clang.sh
@@ -1,3 +1,10 @@
-./autogen.sh
-./configure CC=clang
-scan-build -o ${WORKSPACE}/clangScanBuildReports -v -v --use-cc clang --use-analyzer=/usr/bin/clang make
+#!/bin/bash
+sudo mock -r fedora-26-x86_64 --init
+sudo mock -r fedora-26-x86_64 --install langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config clang clang-analyzer git
+sudo mock -r fedora-26-x86_64 --copyin $WORKSPACE /src
+sudo mock -r fedora-26-x86_64 --chroot "cd /src && ./autogen.sh"
+sudo mock -r fedora-26-x86_64 --chroot "cd /src && ./configure CC=clang --enable-gnfs --enable-debug"
+sudo mock -r fedora-26-x86_64 --chroot "cd /src && scan-build -o /src/clangScanBuildReports -v -v --use-cc clang --use-analyzer=/usr/bin/clang make"
+sudo mock -r fedora-26-x86_64 --copyout /src/clangScanBuildReports $WORKSPACE/clangScanBuildReports
+sudo mock -r fedora-26-x86_64 --clean
+sudo chown -R jenkins:jenkins clangScanBuildReports

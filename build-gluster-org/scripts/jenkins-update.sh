1fca3b4437d6e70e8e704a532c57258a6c3c7cfd
deepshikhaaa <deepshikhakhandelwal8@gmail.com>
2017-08-18 10:51:03 +0530

    Job to update the jenkins server on patch merged
    Change-Id: I92b006d040fd8f5104b287093b97bc509df78627

---
+++ build-gluster-org/scripts/jenkins-update.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+virtualenv env
+env/bin/pip install -e git+git://git.openstack.org/openstack-infra/jenkins-job-builder#egg=jenkins_jobs
+env/jenkins-jobs --conf $JJB_CONFIG update build-gluster-org/jobs

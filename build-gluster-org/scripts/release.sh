dfc79c6cba61160856dd2e01692495aca9748b84
Nigel Babu <nigelb@redhat.com>
2017-12-04 13:12:27 +0530

    Make the release job push to bits.gluster.org
    Change-Id: I0ce6e94bbe6acc0da978493560cbf606f1d237e9

--- build-gluster-org/scripts/release.sh
+++ build-gluster-org/scripts/release.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -xe
-
 ./autogen.sh;
 ./configure --enable-fusermount --enable-gnfs
 make dist
 sha512sum glusterfs-$RELEASE_VERSION.tar.gz > glusterfs-$RELEASE_VERSION.sha512sum
+if [[ "$PUBLISH_ARCHIVES" == "true" ]]; then
+    scp -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i $BITS_KEY glusterfs-$RELEASE_VERSION.tar.gz glusterfs-$RELEASE_VERSION.sha512sum _bits-gluster@http.int.rht.gluster.org:/var/www/bits/pub/gluster/glusterfs/src/
+fi

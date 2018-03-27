fc86b804a1aa4b7d7f28c64e42bf1a4f9c12502d
Nigel Babu <nigelb@redhat.com>
2018-03-27 19:38:44 +0530

    Add concurrency, commit message and module checks
    Change-Id: I65a1006f734314cc1933af0985500a7af21c0f98

--- build-gluster-org/scripts/glusto-lint.sh
+++ build-gluster-org/scripts/glusto-lint.sh
@@ -4,3 +4,19 @@ source /opt/rh/python27/enable
 virtualenv --system-site-packages env
 env/bin/pip install tox
 env/bin/tox
+
+# Commit message standard
+if ! git show --pretty=format:%B | head -n 2 | tail -n 1 | egrep '^$' >/dev/null 2>&1 ; then
+    echo "Bad commit message format! Please add an empty line after the subject line. Do not break subject line with new-lines."
+    exit 1
+fi
+
+# verify that every folder has an __init__.py file
+set +e
+FOLDERLIST=$(find tests/functional -type d -print0 | xargs -0 -I {} ls {}/__init__.py)
+RET=$?
+set -e
+if [ $RET -ne 0 ]; then
+    echo "One of the folders in this change does not have an __init__.py file"
+    exit 1
+fi

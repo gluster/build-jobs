0c78b5732f35b319597ea2b9518f94177a48b4ff
dkhandel <dkhandel@localhost.localdomain>
2018-04-25 14:51:16 +0530

    Job to run the distributed regression testing framework
    Change-Id: Ib3996e81790104068f51b980fd4229dc14c695b3

---
+++ build-gluster-org/scripts/distributed-regression.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import subprocess
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars.manager import VariableManager
+from ansible.inventory.manager import InventoryManager
+
+
+def get_ansible_host_ip():
+   loader = DataLoader()
+   inventory = InventoryManager(loader=loader, sources='hosts')
+   variable_manager = VariableManager(loader=loader, inventory=inventory)
+   hostnames = []
+   for host in inventory.get_hosts():
+       hostnames.append(variable_manager.get_vars(host=host))
+   ip='\t'.join([str(i['ansible_host']) for i in hostnames])
+   return ip
+
+
+def main():
+    ip = get_ansible_host_ip()
+    subprocess.call('/extras/distributed-testing/distributed-test.sh '
+                    '--hosts {0} --id_rsa {1}'.format(ip, ${WORKSPACE}/key))
+
+
+main()

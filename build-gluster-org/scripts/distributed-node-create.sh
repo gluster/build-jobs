b3cd2f3e9e68e9b183dea6d98b8acb9199781deb
dkhandel <dkhandel@localhost.localdomain>
2018-05-23 09:22:56 +0530

    Create the ansible.cfg file for setting private SSH key path
    Change-Id: Iadc7adb17e4e531902260ff6a19f6dc2a92187fb

--- build-gluster-org/scripts/distributed-node-create.sh
+++ build-gluster-org/scripts/distributed-node-create.sh
@@ -11,3 +11,6 @@ ssh-keygen -f ${WORKSPACE}/key -t rsa -b 4096
 # Install pyrax dependency
 pip install pyrax
 /opt/qa/distributed-tests/rackspace-server-manager.py create -n ${MACHINES_COUNT}
+
+#create ansible.cfg to set the private SSH key file path
+echo -e "[defaults]\nprivate_key_file = key" > ansible.cfg

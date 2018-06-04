0c26219bc259d48b7c54e86cec76fcd7244069dc
dkhandel <dkhandel@localhost.localdomain>
2018-06-04 16:30:53 +0530

    Add the key path to subprocess call
    Change-Id: Icd23239670522dde1da83d803c0db398ddd578bf

--- build-gluster-org/scripts/distributed-regression.py
+++ build-gluster-org/scripts/distributed-regression.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import subprocess
+import os
 from ansible.parsing.dataloader import DataLoader
 from ansible.vars.manager import VariableManager
 from ansible.inventory.manager import InventoryManager
@@ -18,8 +19,9 @@ def get_ansible_host_ip():
 
 def main():
     ip = get_ansible_host_ip()
+    key_path = os.path.join(os.environ.get['WORKSPACE'], 'key')
     subprocess.call('/extras/distributed-testing/distributed-test.sh '
-                    '--hosts {0} --id_rsa {1}'.format(ip, key))
+                    '--hosts {0} --id_rsa {1}'.format(ip, key_path))
 
 
 main()

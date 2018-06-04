82e6503217ecd7e50d1b515656f12e5bc20babf1
dkhandel <dkhandel@localhost.localdomain>
2018-06-04 14:11:27 +0530

    Fix the bug in distributed-regression.py file
    Change-Id: I6075d31981fad730684f4b025c94121bd8c503e6

--- build-gluster-org/scripts/distributed-regression.py
+++ build-gluster-org/scripts/distributed-regression.py
@@ -19,7 +19,7 @@ def get_ansible_host_ip():
 def main():
     ip = get_ansible_host_ip()
     subprocess.call('/extras/distributed-testing/distributed-test.sh '
-                    '--hosts {0} --id_rsa {1}'.format(ip, ${WORKSPACE}/key))
+                    '--hosts {0} --id_rsa {1}'.format(ip, key))
 
 
 main()

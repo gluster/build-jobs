561ed8834a8e97ed6bf1f125701ce5d0fd1c19f1
dkhandel <dkhandel@localhost.localdomain>
2018-06-08 11:27:15 +0530

    Install the ansible module in virtual environment
    Change-Id: Ifbf47d685e620ae70538c596eece59bba6ad5ab2

--- build-gluster-org/scripts/distributed-regression.sh
+++ build-gluster-org/scripts/distributed-regression.sh
@@ -9,8 +9,8 @@ source env/bin/activate
 # create SSH key pair of 4096 bits to use it for instances at Rackspace
 ssh-keygen -f ${WORKSPACE}/key -t rsa -b 4096
 
-# Install pyrax dependency
-pip install pyrax
+# Install dependencies
+pip install pyrax ansible
 /opt/qa/distributed-tests/rackspace-server-manager.py create -n ${MACHINES_COUNT}
 
 for retry in `seq 1 $MAX_ATTEMPTS`

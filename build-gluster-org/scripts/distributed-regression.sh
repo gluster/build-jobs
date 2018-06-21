4ff6c1f6b848e5ca76ec82194ef67e9fb507db08
dkhandel <dkhandel@localhost.localdomain>
2018-06-21 14:30:46 +0530

    Exit the code with return value $ret
    Fail the build if there is any failed test with  exit code
    $ret and copy the failed test logs to http.int.rht.gluster.org
    
    Change-Id: If4dc1c809311b609d2babe2b04dd7dd799460d6e
    Signed-off-by: Deepshikha Khandelwal <dkhandel@redhat.com>

--- build-gluster-org/scripts/distributed-regression.sh
+++ build-gluster-org/scripts/distributed-regression.sh
@@ -4,16 +4,18 @@ MAX_ATTEMPTS=3
 
 # create and activate virtual env
 virtualenv env
-source env/bin/activate
+. env/bin/activate
 
 # create SSH key pair of 4096 bits to use it for instances at Rackspace
-ssh-keygen -f ${WORKSPACE}/key -t rsa -b 4096
+ssh-keygen -f "${WORKSPACE}"/key -t rsa -b 4096
 
 # Install dependencies
 pip install pyrax ansible
-/opt/qa/distributed-tests/rackspace-server-manager.py create -n ${MACHINES_COUNT}
 
-for retry in `seq 1 $MAX_ATTEMPTS`
+#create the server maachines
+/opt/qa/distributed-tests/rackspace-server-manager.py create -n "${MACHINES_COUNT}"
+
+for retry in $(seq 1 $MAX_ATTEMPTS)
 do
   ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts --private-key=key /opt/qa/distributed-tests/distributed-server.yml -u root
   ret=$?
@@ -26,15 +28,15 @@ done
 # run the script of distributed-test
 /opt/qa/distributed-tests/run-distributed-test.py
 ret=$?
-if [ $ret -eq 0 ]; then
-  # Create tar file from all the failed test log files generated in /tmp
-  tar -czf $WORKSPACE/failed-tests-logs.tgz /tmp/*.log
-  scp -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i $LOG_KEY failed-tests-logs.tgz "_logs-collector@http.int.rht.gluster.org:/var/www/glusterfs-logs/$JOB_NAME-logs-$BUILD_ID.tgz" || true;
-  echo "Failed tests logs stored in https://ci-logs.gluster.org/$JOB_NAME-logs-$BUILD_ID.tgz"
 
-  # if test runs are successful, delete all the machines
-  /opt/qa/distributed-tests/rackspace-server-manager.py delete
-  exit 0
-else
+#delete the server machines
+/opt/qa/distributed-tests/rackspace-server-manager.py delete
+if [ $ret -ne 0 ]; then
+  # Create tar file from all the failed test log files generated in /tmp
+  tar -czf "$WORKSPACE"/failed-tests-logs.tgz /tmp/*.log
+  scp -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i "$LOG_KEY" failed-tests-logs.tgz "_logs-collector@http.int.rht.gluster.org:/var/www/glusterfs-logs/$JOB_NAME-logs-$BUILD_ID.tgz" || true;
+  echo "Failed tests logs stored in https://ci-logs.gluster.org/$JOB_NAME-logs-$BUILD_ID.tgz"
   exit $ret
+else
+  exit 0
 fi

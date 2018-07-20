d3037d0a52faf8cc0bf351e5c6af34cc8b53e97e
dkhandel <dkhandel@localhost.localdomain>
2018-07-20 16:45:38 +0530

    Compile the gluster code with GCC8 on Fedora
    Change-Id: I64c6b79e9416a0e602426450996e96f1f9da300b
    Signed-off-by: Deepshikha Khandelwal <dkhandel@redhat.com>

---
+++ build-gluster-org/scripts/fedora-smoke.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+SRC=$(pwd);
+nproc=$(getconf _NPROCESSORS_ONLN)
+
+./autogen.sh;
+P=/build;
+rm -rf $P/scratch;
+mkdir -p $P/scratch;
+cd $P/scratch;
+rm -rf $P/install;
+$SRC/configure --prefix=$P/install --with-mountutildir=$P/install/sbin \
+               --with-initdir=$P/install/etc --localstatedir=/var \
+               --enable-bd-xlator=yes --enable-debug --enable-gnfs --silent
+make CFLAGS="-Wall -Werror -Wno-cpp" -j ${nproc};

03614883897903152688f9f6ea4deb129c8fcc6c
Nigel Babu <nigelb@redhat.com>
2018-08-17 07:29:46 +0530

    Add a job for gd2 containers
    Change-Id: I42ebd35471af8e0b7b36d1039be48a28912f409f

---
+++ build-gluster-org/scripts/glusterd2-containers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+IMG_NAME="gluster/glusterd2-nightly"
+IMG_VERSION=$(date +%Y%m%d)
+
+cd extras/nightly-container
+./build.sh
+if [ "$PUSH_TO_HUB" = true ]; then
+    buildah push --authfile $AUTH_JSON "localhost/$IMG_NAME:$IMG_VERSION" "docker://docker.io/gluster/glusterd2-nightly:$IMG_VERSION"
+    buildah push --authfile $AUTH_JSON "localhost/$IMG_NAME:$IMG_VERSION" "docker://docker.io/gluster/glusterd2-nightly:latest"
+fi

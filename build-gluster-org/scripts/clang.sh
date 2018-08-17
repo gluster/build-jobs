f5db035f35b9fbf03b66b160ad0efb41739e19c2
Amar Tumballi <amarts@redhat.com>
2018-08-17 13:30:50 +0530

    clang: remove block-device from the build
    Change-Id: If6a16648d832443f428ee5d1d656febae16656e0
    Signed-off-by: Amar Tumballi <amarts@redhat.com>

--- build-gluster-org/scripts/clang.sh
+++ build-gluster-org/scripts/clang.sh
@@ -3,7 +3,7 @@ sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --init
 sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --install langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config clang clang-analyzer git
 sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --copyin $WORKSPACE /src
 sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --chroot "cd /src && ./autogen.sh"
-sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --chroot "cd /src && ./configure CC=clang --enable-gnfs --enable-debug"
+sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --chroot "cd /src && ./configure CC=clang --enable-gnfs --disable-bd --enable-debug"
 sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --chroot "cd /src && scan-build -o /src/clangScanBuildReports -disable-checker deadcode.DeadStores -v -v --use-cc clang --use-analyzer=/usr/bin/clang make"
 sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --copyout /src/clangScanBuildReports $WORKSPACE/clangScanBuildReports
 sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --clean

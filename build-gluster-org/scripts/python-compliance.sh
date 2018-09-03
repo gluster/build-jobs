55f1949776a56140b3fb8247a5d680bb29359308
Nigel Babu <nigelb@redhat.com>
2018-09-03 12:24:23 +0530

    Add a python-ast check as well
    Change-Id: I1ac7d514b4e4022845329d423cab60f2200dc546

--- build-gluster-org/scripts/python-compliance.sh
+++ build-gluster-org/scripts/python-compliance.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
+./autogen.sh
+./configure --disable-bd-xlator --enable-debug --enable-gnfs --silent
 grep -rnl '#!/usr/bin/python' | xargs pylint-3 --py3k
 find . -name '*.py' | xargs pylint-3 --py3k
-find . -name '*.in' | xargs file | grep 'Python' | awk '{print $1}' | cut -d: -f 1 | xargs pylint-3 --py3k
+find . -name '*.py' | xargs python2 /opt/qa/python_compliance.py
+find . -name '*.py' | xargs python3 /opt/qa/python_compliance.py

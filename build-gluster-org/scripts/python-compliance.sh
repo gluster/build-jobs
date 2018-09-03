65ec08b624e3966b1da7baacdea3e4347e188962
Nigel Babu <nigelb@redhat.com>
2018-09-03 21:18:10 +0530

    Fix bugs in python-compliance tests
    Change-Id: I2f19695308549265e4a531443b13d153d3f911ed

--- build-gluster-org/scripts/python-compliance.sh
+++ build-gluster-org/scripts/python-compliance.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
+set -e
 ./autogen.sh
 ./configure --disable-bd-xlator --enable-debug --enable-gnfs --silent
-grep -rnl '#!/usr/bin/python' | xargs pylint-3 --py3k
+grep -rnl --exclude-dir='.git' '#!/usr/bin/python' | xargs pylint-3 --py3k
 find . -name '*.py' | xargs pylint-3 --py3k
 find . -name '*.py' | xargs python2 /opt/qa/python_compliance.py
 find . -name '*.py' | xargs python3 /opt/qa/python_compliance.py

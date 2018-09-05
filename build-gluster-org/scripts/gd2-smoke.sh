9bef12440ed52ca8a8aa9e9b2920f0264cf24ba3
Nigel Babu <nigelb@redhat.com>
2018-09-05 18:24:37 +0530

    Add the clean up steps we do on every job
    Change-Id: I4b7b0362fc9f78fdbb42c7d9d94b78b402df74f5

--- build-gluster-org/scripts/gd2-smoke.sh
+++ build-gluster-org/scripts/gd2-smoke.sh
@@ -2,8 +2,16 @@
 
 set -e
 
+JDIRS="/var/log/glusterfs /var/lib/glusterd /var/lib/glusterd/groups/virt /var/run/gluster /d /d/archived_builds /d/backends /d/build /d/logs /home/jenkins/root /build/*"
+sudo mkdir -p $JDIRS
+echo Return code = $?
+sudo chown -RH jenkins:jenkins $JDIRS
+echo Return code = $?
+sudo chmod -R 755 $JDIRS
+echo Return code = $?
+
 # build the glusterfs source code
 /opt/qa/build.sh
 
-# run gd2 unit tests script
+# run gd2 tests script
 /opt/qa/glusterd2-test.sh

16b96cebdfa35be8a01bb899605138a02bdf6fdc
Nigel Babu <nigelb@redhat.com>
2018-09-10 13:23:15 +0530

    Fix the directories for gluster
    Change-Id: I9a1467e1cad2483b4fd97f5cb4adb5eefb00b28c

--- build-gluster-org/scripts/gd2-smoke.sh
+++ build-gluster-org/scripts/gd2-smoke.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-JDIRS="/var/log/glusterfs /var/lib/glusterd /var/lib/glusterd/groups/virt /var/run/gluster /d /d/archived_builds /d/backends /d/build /d/logs /home/jenkins/root /build/*"
+JDIRS="/var/log/glusterfs /var/lib/glusterd /var/lib/glusterd/groups/virt /var/run/gluster /home/jenkins/root /build/*"
 sudo rm -rf $JDIRS
 sudo mkdir -p $JDIRS
 echo Return code = $?

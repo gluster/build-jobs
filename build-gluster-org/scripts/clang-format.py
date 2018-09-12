e12e424e36cdb1ba888b35a5787b488707238f08
Nigel Babu <nigelb@redhat.com>
2018-09-12 19:09:58 +0530

    Make clang-format-job fail for real
    Change-Id: I17c4e410d66749c3a661ddb695e57a197cdf1078

--- build-gluster-org/scripts/clang-format.py
+++ build-gluster-org/scripts/clang-format.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import subprocess
-output = subprocess.check_output(["git", "clang-format", "--diff"])
+output = subprocess.check_output(["git-clang-format", "HEAD~", "--diff"])
 
 if output not in ['no modified files to format\n', 'clang-format did not modify any files\n']:
     print "Run git clang-format, then commit.\n"

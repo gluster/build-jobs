e6b2fc7012dba75199a4fbb3767178112b4b2bcf
Nigel Babu <nigelb@redhat.com>
2018-09-13 14:47:21 +0530

    clang-format: Be more verbose
    Change-Id: I2e89594b0c7f2501958bfedf3ae5a23242b0b92b

--- build-gluster-org/scripts/clang-format.py
+++ build-gluster-org/scripts/clang-format.py
@@ -3,8 +3,10 @@
 import subprocess
 output = subprocess.check_output(["git-clang-format", "HEAD~", "--diff"])
 
-if output not in ['no modified files to format\n', 'clang-format did not modify any files\n']:
-    print "Run git clang-format, then commit.\n"
+if output not in ['no modified files to format\n',
+        'clang-format did not modify any files\n']:
+    print(output)
+    print("The above patch to be applied to pass clang-format")
     exit(1)
 else:
     exit(0)

0ecf037e4da0e68e9cd05b964d9f8bbdf7df1578
Nigel Babu <nigelb@redhat.com>
2019-01-03 13:27:06 +0530

    Re-enable coverity and fix failures in gd2 job
    Change-Id: Iad05a5f1117683d1165ab3099d68fa4ca8218c5e

--- build-gluster-org/scripts/glusterd2-containers.sh
+++ build-gluster-org/scripts/glusterd2-containers.sh
@@ -8,4 +8,4 @@ if [ "$PUSH_TO_HUB" = true ]; then
     buildah push --authfile $AUTH_JSON "localhost/$IMG_NAME:$IMG_VERSION" "docker://docker.io/gluster/glusterd2-nightly:$IMG_VERSION"
     buildah push --authfile $AUTH_JSON "localhost/$IMG_NAME:$IMG_VERSION" "docker://docker.io/gluster/glusterd2-nightly:latest"
 fi
-buildah rmi --all
+buildah rmi --all || true

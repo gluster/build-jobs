f3da23ce1972da5cebbd8b00064557dfeca57d39
Michael Scherer <misc@redhat.com>
2019-01-29 10:33:44 +0100

    Do not copy the whole directory
    For some reason, git do create file without write permission, so
    subsequent sync fail. By copying only the generated file, we avoid the
    issue, even if this mean keeping the list of file to sync up to
    date in that job.
    
    Change-Id: Id3aac8080397ce9849fbd01aeb07fdf0d1b742cb

--- build-gluster-org/scripts/bugs-summary.sh
+++ build-gluster-org/scripts/bugs-summary.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./run-report.sh
 
-scp -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i "$LOG_KEY" -r . _bits-gluster@http.int.rht.gluster.org:/var/www/glusterfs-bugs
+scp -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i "$LOG_KEY" -r gluster-bugs.html gluster-bugs.js bugs.json style.css _bits-gluster@http.int.rht.gluster.org:/var/www/glusterfs-bugs

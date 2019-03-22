993a39d4e848f54cc1cdfcff12a929d8ad106ea9
Michael Scherer <misc@redhat.com>
2019-03-22 12:40:25 +0100

    Refactor the script
    ease the change of chroot and options
    
    Change-Id: I1f7c442d383208507620d79fbb491b097f490938

--- build-gluster-org/scripts/clang.sh
+++ build-gluster-org/scripts/clang.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --init
-sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --install langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config clang clang-analyzer git
-sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --copyin $WORKSPACE /src
-sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --chroot "cd /src && ./autogen.sh"
-sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --chroot "cd /src && ./configure CC=clang --enable-gnfs --disable-bd-xlator --enable-debug"
-sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --chroot "cd /src && scan-build -o /src/clangScanBuildReports -disable-checker deadcode.DeadStores -v -v --use-cc clang --use-analyzer=/usr/bin/clang make"
-sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --copyout /src/clangScanBuildReports $WORKSPACE/clangScanBuildReports
-sudo mock -r fedora-27-x86_64 --config-opts=dnf_warning=False --clean
+MOCK_CHROOT=fedora-27-x86_64
+MOCK="sudo mock -r $MOCK_CHROOT --config-opts=dnf_warning=False"
+
+$MOCK --init
+$MOCK --install langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config clang clang-analyzer git
+$MOCK --copyin $WORKSPACE /src
+$MOCK --chroot "cd /src && ./autogen.sh"
+$MOCK --chroot "cd /src && ./configure CC=clang --enable-gnfs --disable-bd-xlator --enable-debug"
+$MOCK --chroot "cd /src && scan-build -o /src/clangScanBuildReports -disable-checker deadcode.DeadStores -v -v --use-cc clang --use-analyzer=/usr/bin/clang make"
+$MOCK --copyout /src/clangScanBuildReports $WORKSPACE/clangScanBuildReports
+$MOCK --clean
 sudo chown -R jenkins:jenkins clangScanBuildReports

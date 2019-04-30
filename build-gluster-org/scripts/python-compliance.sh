56d6b135fa3e7145ac87ffffd9ce656ee0b905b1
Amar Tumballi <amarts@redhat.com>
2019-04-30 09:34:39 +0530

    scripts: remove deprecated bd-xlator option
    Change-Id: Ic7756d3b03539a9e36c8995c00705d635618c87d
    Signed-off-by: Amar Tumballi <amarts@redhat.com>

--- build-gluster-org/scripts/python-compliance.sh
+++ build-gluster-org/scripts/python-compliance.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 set -e
 ./autogen.sh
-./configure --disable-bd-xlator --enable-debug --enable-gnfs --silent
+./configure --enable-debug --enable-gnfs --silent
 grep -rnl --exclude-dir='.git' '#!/usr/bin/python' | xargs pylint-3 --py3k
 find . -name '*.py' | xargs pylint-3 --py3k
 find . -name '*.py' | xargs python2 /opt/qa/python_compliance.py

f5068f3f43b6227888b4e9d85393cb903d0fa0af
Niels de Vos <ndevos@redhat.com>
2019-05-15 14:55:42 +0200

    the jenkins-job-builder git repository moved to opendev.org
    Cloning the JJB repo from git.openstack.org fails. The new home for the
    repository seems to be on opendev.org (git.openstack.org redirects
    there).
    
    Change-Id: I00cbe3c505343e55433aab35e4960fece7faf779
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

--- build-gluster-org/scripts/jenkins-update.sh
+++ build-gluster-org/scripts/jenkins-update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 virtualenv env
-env/bin/pip install -e git+https://git.openstack.org/openstack-infra/jenkins-job-builder#egg=jenkins_jobs
+env/bin/pip install -e git+https://opendev.org/jjb/jenkins-job-builder.git#egg=jenkins_jobs
 env/bin/jenkins-jobs --conf $JJB_CONFIG update build-gluster-org/jobs

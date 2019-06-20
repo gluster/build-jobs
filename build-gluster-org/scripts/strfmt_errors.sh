8f1051f6da0398832ce13abff945cec8b9bf3aee
Amar Tumballi <amarts@redhat.com>
2019-06-20 14:01:50 +0530

    strfmt-script: move to epel-7 for mock
    Change-Id: Ia727fd3fcfea45806b2b554bcb1852d831ed1199
    Signed-off-by: Amar Tumballi <amarts@redhat.com>

--- build-gluster-org/scripts/strfmt_errors.sh
+++ build-gluster-org/scripts/strfmt_errors.sh
@@ -3,7 +3,7 @@
 ./configure --enable-fusermount || exit 1
 cd extras/LinuxRPM
 make prep srcrpm || exit 1
-sudo mock -r 'epel-6-i386' --resultdir=${WORKSPACE}/RPMS/"%(dist)s"/"%(target_arch)s"/ --cleanup-after --rebuild glusterfs*src.rpm || exit 1
+sudo mock -r 'epel-7-i386' --resultdir=${WORKSPACE}/RPMS/"%(dist)s"/"%(target_arch)s"/ --cleanup-after --rebuild glusterfs*src.rpm || exit 1
 
 rm -f warnings.txt
 grep -E ".*: warning: format '%.*' expects( argument of)? type '.*', but argument .* has type 'ssize_t" ${WORKSPACE}/RPMS/el6/i686/build.log | tee -a warnings.txt

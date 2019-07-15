0d6b1f896a76aaf32794dd6ec60606a49080eddb
Deepshikha khandelwal <dkhandel@redhat.com>
2019-07-15 14:27:26 +0530

    Add sudo when creating nad cleaning up the directories
    Change-Id: If7f8f193fbbf539476011589a68a8ed0961f2b11
    Signed-off-by: Deepshikha khandelwal <dkhandel@redhat.com>

--- build-gluster-org/scripts/fedora-smoke.sh
+++ build-gluster-org/scripts/fedora-smoke.sh
@@ -7,10 +7,10 @@ nproc=$(getconf _NPROCESSORS_ONLN)
 
 ./autogen.sh;
 P=/build;
-rm -rf $P/scratch;
-mkdir -p $P/scratch;
+sudo rm -rf $P/scratch;
+sudo mkdir -p $P/scratch;
 cd $P/scratch;
-rm -rf $P/install;
+sudo rm -rf $P/install;
 $SRC/configure --prefix=$P/install --with-mountutildir=$P/install/sbin \
                --with-initdir=$P/install/etc --localstatedir=/var \
                --enable-debug --enable-gnfs --silent

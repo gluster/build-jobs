6ea0c512a89c251a1e37397a17f39281d2a31409
Deepshikha Khandelwal <dkhandel@redhat.com>
2020-01-17 09:57:39 +0530

    Bug fix the build flag to F30 instead of F28
    Change-Id: I7dba65c7f68f2e30c19bfcda30234ee3870e3842

--- build-gluster-org/scripts/32-bit-build-smoke.sh
+++ build-gluster-org/scripts/32-bit-build-smoke.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 nproc=$(getconf _NPROCESSORS_ONLN)
-sudo mock -r fedora-28-i386 --config-opts=dnf_warning=False --init
-sudo mock -r fedora-28-i386 --config-opts=dnf_warning=False --install langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config clang clang-analyzer git rpcgen libtirpc-devel
-sudo mock -r fedora-28-i386 --config-opts=dnf_warning=False --copyin $WORKSPACE /src
-sudo mock -r fedora-28-i386 --config-opts=dnf_warning=False --chroot "cd /src && ./autogen.sh"
-sudo mock -r fedora-28-i386 --config-opts=dnf_warning=False --chroot "cd /src && ./configure --enable-gnfs --enable-debug"
-sudo mock -r fedora-28-i386 --config-opts=dnf_warning=False --chroot "cd src && make install CFLAGS='-Wall -Werror' -j ${nproc}"
+sudo mock -r fedora-30-i386 --config-opts=dnf_warning=False --init
+sudo mock -r fedora-30-i386 --config-opts=dnf_warning=False --install langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config clang clang-analyzer git rpcgen libtirpc-devel
+sudo mock -r fedora-30-i386 --config-opts=dnf_warning=False --copyin $WORKSPACE /src
+sudo mock -r fedora-30-i386 --config-opts=dnf_warning=False --chroot "cd /src && ./autogen.sh"
+sudo mock -r fedora-30-i386 --config-opts=dnf_warning=False --chroot "cd /src && ./configure --enable-gnfs --enable-debug"
+sudo mock -r fedora-30-i386 --config-opts=dnf_warning=False --chroot "cd src && make install CFLAGS='-Wall -Werror' -j ${nproc}"
 ret=$?
-sudo mock -r fedora-28-i386 --config-opts=dnf_warning=False --clean
+sudo mock -r fedora-30-i386 --config-opts=dnf_warning=False --clean
 exit $ret

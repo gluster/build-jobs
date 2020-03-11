87598d2a4faa7a5a54ed283a72b100a210197653
Michael Scherer <misc@redhat.com>
2020-03-11 12:01:15 +0100

    Add a job to generate planet.gluster.org
    Change-Id: Ib520fd99acd0f816bcdf62827269a85fb2bdb924

---
+++ build-gluster-org/scripts/planet.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+MOCK_CHROOT=fedora-30-x86_64
+
+MOCK="sudo mock -r $MOCK_CHROOT --config-opts=dnf_warning=False --config-opts=use_bootstrap_container=True"
+
+$MOCK --clean
+$MOCK --init
+$MOCK --install rubygem-bundler ruby-devel curl-devel make gcc gcc-c++ ImageMagick patch zlib-devel tar git rubygem-bigdecimal
+$MOCK --copyin $WORKSPACE /src
+$MOCK --enable-network --chroot "cd /src && bundle install && bundle exec middleman build --verbose"
+$MOCK --copyout /src/build/ $WORKSPACE/build
+$MOCK --clean
+
+sudo chown -R jenkins:jenkins build
+scp -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i "$LOG_KEY" -r build _bits-gluster@http.int.rht.gluster.org:/var/www/glusterfs-planet

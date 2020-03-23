aa1c1575975dae3899788b2efe828003ac57f2a6
Deepshikha Khandelwal <dkhandel@redhat.com>
2020-03-23 10:42:43 +0530

    Remove untriaged bugzilla jenkins jobs (moved out of Bugzilla)
    Change-Id: I7c2ecaade5235ea3e470271e1ee38da9c7bf0de5

--- build-gluster-org/scripts/untriaged-bugs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-echo "Weekly GlusterFS Untriaged Bugs Report"
-echo ""
-bugzilla query --outputformat='https://bugzilla.redhat.com/%{id} / %{component}: %{summary}' --from-url='https://bugzilla.redhat.com/buglist.cgi?bug_status=NEW&product=GlusterFS&chfield=[Bug creation]&chfieldfrom=-4w&chfieldto=Now&f1=keywords&o1=notsubstring&v1=Triaged' | sort -k 3

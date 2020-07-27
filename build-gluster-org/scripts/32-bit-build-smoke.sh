1b08c1e4afcf66cc36b8b0028e82a27703e40797
Michael Scherer <misc@redhat.com>
2020-07-27 19:57:43 +0200

    Refactor the script
    While this should be shared with others jobs, there is no easy way as far as I know

--- build-gluster-org/scripts/32-bit-build-smoke.sh
+++ build-gluster-org/scripts/32-bit-build-smoke.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+MOCK_CHROOT="fedora-30-i386"
 
+MOCK="sudo mock -r $MOCK_CHROOT --config-opts=dnf_warning=False"
 nproc=$(getconf _NPROCESSORS_ONLN)
-sudo mock -r fedora-30-i386 --config-opts=dnf_warning=False --init
-sudo mock -r fedora-30-i386 --config-opts=dnf_warning=False --install langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config clang clang-analyzer git rpcgen libtirpc-devel
-sudo mock -r fedora-30-i386 --config-opts=dnf_warning=False --copyin $WORKSPACE /src
-sudo mock -r fedora-30-i386 --config-opts=dnf_warning=False --chroot "cd /src && ./autogen.sh"
-sudo mock -r fedora-30-i386 --config-opts=dnf_warning=False --chroot "cd /src && ./configure --enable-gnfs --enable-debug"
-sudo mock -r fedora-30-i386 --config-opts=dnf_warning=False --chroot "cd src && make install CFLAGS='-Wall -Werror -Wno-address-of-packed-member' -j ${nproc}"
+
+$MOCK --init
+$MOCK --install langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config clang clang-analyzer git rpcgen libtirpc-devel
+$MOCK --copyin $WORKSPACE /src
+$MOCK --chroot "cd /src && ./autogen.sh"
+$MOCK --chroot "cd /src && ./configure --enable-gnfs --enable-debug"
+$MOCK --chroot "cd src && make install CFLAGS='-Wall -Werror -Wno-address-of-packed-member' -j ${nproc}"
 ret=$?
-sudo mock -r fedora-30-i386 --config-opts=dnf_warning=False --clean
+$MOCK --clean
 exit $ret

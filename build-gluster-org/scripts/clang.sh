92e430f8cb44c7f85bd001fa8a9968857c5db1b8
Michael Scherer <misc@redhat.com>
2020-10-20 16:19:07 +0200

    Add missing deps, libuuid-devel
    See gluster/project-infrastructure/issues/90

--- build-gluster-org/scripts/clang.sh
+++ build-gluster-org/scripts/clang.sh
@@ -2,7 +2,7 @@
 MOCK="sudo mock -r $MOCK_CHROOT --config-opts=dnf_warning=False"
 
 $MOCK --init
-$MOCK --install rpcgen libtirpc-devel langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config clang clang-analyzer git
+$MOCK --install rpcgen libtirpc-devel langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel libuuid-devel redhat-rpm-config clang clang-analyzer git
 $MOCK --copyin $WORKSPACE /src
 $MOCK --chroot "cd /src && ./autogen.sh"
 $MOCK --chroot "cd /src && ./configure CC=clang --enable-gnfs --enable-debug"

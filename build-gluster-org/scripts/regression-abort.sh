80bbaa6447efe0ad01c5791e836a22acf4580422
Michael Scherer <misc@redhat.com>
2020-11-06 11:59:23 +0100

    Cleanup the builder when the run is aborted

--- build-gluster-org/scripts/regression-abort.sh
+++ build-gluster-org/scripts/regression-abort.sh
@@ -8,4 +8,9 @@ SERVER=$(hostname)
 filename="${ARCHIVED_LOGS}/glusterfs-logs-${UNIQUE_ID}.tgz"
 sudo -E tar -czf "${ARCHIVE_BASE}/${filename}" /var/log/glusterfs /var/log/messages*;
 echo "Logs archived in http://${SERVER}/${filename}"
+# do clean up after a regression test suite is run
+sudo -E bash /opt/qa/cleanup.sh
+# make sure that every file/diretory belongs to jenkins
+sudo chown -R jenkins:jenkins $WORKSPACE
+
 sudo reboot

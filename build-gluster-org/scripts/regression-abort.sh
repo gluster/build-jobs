298df78ed04d4df21828c2ac76983ae71b32c81b
Deepshikha Khandelwal <dkhandel@redhat.com>
2020-11-19 14:58:43 +0530

    Get logs for aborted regression job

--- build-gluster-org/scripts/regression-abort.sh
+++ build-gluster-org/scripts/regression-abort.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-ARCHIVE_BASE="/archives"
-ARCHIVED_LOGS="logs"
-UNIQUE_ID="${JOB_NAME}-${BUILD_ID}"
-SERVER=$(hostname)
-
-filename="${ARCHIVED_LOGS}/glusterfs-logs-${UNIQUE_ID}.tgz"
-sudo -E tar -czf "${ARCHIVE_BASE}/${filename}" /var/log/glusterfs /var/log/messages*;
-echo "Logs archived in http://${SERVER}/${filename}"
+sudo -E tar -czf $WORKSPACE/glusterfs-logs.tgz /var/log/glusterfs /var/log/messages*;
+echo "Logs are archived at Build artifacts: https://build.gluster.org/job/${JOB_NAME}/${UNIQUE_ID}"
 # do clean up after a regression test suite is run
 sudo -E bash /opt/qa/cleanup.sh
 # make sure that every file/diretory belongs to jenkins

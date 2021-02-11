6550676706da9ba423fb3e4f982bad8d86fdcb40
Shwetha K Acharya <sacharya@redhat.com>
2021-02-11 10:29:14 +0530

    refactor loops
    Signed-off-by: Shwetha K Acharya <sacharya@redhat.com>

--- build-gluster-org/scripts/debian-ubuntu-package.sh
+++ build-gluster-org/scripts/debian-ubuntu-package.sh
@@ -5,19 +5,21 @@
 
 set -xe
 
+deb_flavors=(stretch buster bullseye)
+ub_flavors=(bionic groovy xenial focal hirsute)
+
 while [ $# -eq 5 ]
 do
         echo "building everything"
         echo "packing debian distribution"
-        ~/build-gluster-org/scripts/generic_package.sh debian stretch $SERIES $VERSION $RELEASE $LATEST_SERIES $LATEST_VERSION
-        ~/build-gluster-org/scripts/generic_package.sh debian buster $SERIES $VERSION $RELEASE
-        ~/build-gluster-org/scripts/generic_package.sh debian bullseye $SERIES $VERSION $RELEASE
+        for i in ${!deb_flavors[@]}; do
+        ~/build-gluster-org/scripts/generic_package.sh debian ${deb_flavors[$i]} $SERIES $VERSION $RELEASE $LATEST_SERIES $LATEST_VERSION
+        done
+
         echo "packing ubuntu distribution"
-        ~/build-gluster-org/scripts/generic_package.sh ubuntu xenial $SERIES $VERSION $RELEASE
-        ~/build-gluster-org/scripts/generic_package.sh ubuntu bionic $SERIES $VERSION $RELEASE
-        ~/build-gluster-org/scripts/generic_package.sh ubuntu disco $SERIES $VERSION $RELEASE
-        ~/build-gluster-org/scripts/generic_package.sh ubuntu eoan $SERIES $VERSION $RELEASE
-        ~/build-gluster-org/scripts/generic_package.sh ubuntu focal $SERIES $VERSION $RELEASE
+        for i in ${!ub_flavors[@]}; do
+        ~/build-gluster-org/scripts/generic_package.sh debian ${ub_flavors[$i]} $SERIES $VERSION $RELEASE $LATEST_SERIES $LATEST_VERSION
+        done
 done
 
 while [ $# -gt 5 ]
@@ -25,52 +27,34 @@ do
     if [ "$OS" == "all" ]; then
         echo "packing all distribution"
         echo "packing debian distribution"
-        ~/build-gluster-org/scripts/generic_package.sh debian stretch $SERIES $VERSION $RELEASE $LATEST_SERIES $LATEST_VERSION
-        ~/build-gluster-org/scripts/generic_package.sh debian buster $SERIES $VERSION $RELEASE
-        ~/build-gluster-org/scripts/generic_package.sh debian bullseye $SERIES $VERSION $RELEASE
+        flavors=(stretch buster bullseye)
+        for i in ${!deb_flavors[@]}; do
+        ~/build-gluster-org/scripts/generic_package.sh debian ${deb_flavors[$i]} $SERIES $VERSION $RELEASE $LATEST_SERIES $LATEST_VERSION
+        done
+
         echo "packing ubuntu distribution"
-        ~/build-gluster-org/scripts/generic_package.sh ubuntu xenial $SERIES $VERSION $RELEASE
-        ~/build-gluster-org/scripts/generic_package.sh ubuntu bionic $SERIES $VERSION $RELEASE
-        ~/build-gluster-org/scripts/generic_package.sh ubuntu disco $SERIES $VERSION $RELEASE
-        ~/build-gluster-org/scripts/generic_package.sh ubuntu eoan $SERIES $VERSION $RELEASE
-        ~/build-gluster-org/scripts/generic_package.sh ubuntu focal $SERIES $VERSION $RELEASE
+        flavors=(bionic groovy xenial focal hirsute)
+        for i in ${!ub_flavors[@]}; do
+        ~/build-gluster-org/scripts/generic_package.sh debian ${ub_flavors[$i]} $SERIES $VERSION $RELEASE $LATEST_SERIES $LATEST_VERSION
+        done
+
     elif [ "$OS" == "debian" ]; then
         echo "packing debian alone"
-        if [ "$FLAVOR" == "stretch" ] || [ "$FLAVOR" == "9" ]; then
-        echo "packing debian stretch alone"
+        case $FLAVOR in
+        "stretch" | "9" | "buster" | "10" | "bullseye" | "11")
+        echo "packing debian ${FLAVOR} alone"
         ../scripts/generic_package.sh $OS $FLAVOR $SERIES $VERSION $RELEASE $LATEST_SERIES $LATEST_VERSION
-        fi
-        if [ "$FLAVOR" == "buster" ] || [ "$FLAVOR" == "10" ]; then
-        echo "packing debian buster alone"
-        ../scripts/generic_package.sh $OS $FLAVOR $SERIES $VERSION $RELEASE $LATEST_SERIES $LATEST_VERSION
-        fi
-        if [ "$FLAVOR" == "bullseye" ] || [ "$FLAVOR" == "11" ]; then
-        echo "packing debian bullseye alone"
-        ../scripts//generic_package.sh $OS $FLAVOR $SERIES $VERSION $RELEASE $LATEST_SERIES $LATEST_VERSION
-        fi
+        ;;
+        esac
+
     elif [ "$OS" == "ubuntu" ]; then
         echo "packing ubuntu alone"
-        if [ "$FLAVOR" == "xenial" ] || [ "$FLAVOR" == "16.04" ]; then
-        echo "packing xenial alone"
+        case $FLAVOR in
+        "xenial" | "16.04" | "bionic" | "18.04" | "eoan" | "19.10" | "focal" | "Focal Fossa" | "20.04" | "hirsute" | "21.04" | "hirsute hippo")
+        echo "packing ubuntu ${FLAVOR} alone"
         ../scripts/generic_package.sh $OS $FLAVOR $SERIES $VERSION $RELEASE $LATEST_SERIES $LATEST_VERSION
-        fi
-        if [ "$FLAVOR" == "bionic" ] || [ "$FLAVOR" == "18.04" ]; then
-        echo "packing bionic alone"
-        ../scripts/generic_package.sh $OS $FLAVOR $SERIES $VERSION $RELEASE $LATEST_SERIES $LATEST_VERSION
-        fi
-        if [ "$FLAVOR" == "disco" ] || [ "$FLAVOR" == "19.04" ]; then
-        echo "packing disco alone"
-        ../scripts/generic_package.sh $OS $FLAVOR $SERIES $VERSION $RELEASE $LATEST_SERIES $LATEST_VERSION
-        fi
-        if [ "$FLAVOR" == "eoan" ] || [ "$FLAVOR" == "19.10" ]; then
-        echo "packing eoan alone"
-        ../scripts/generic_package.sh $OS $FLAVOR $SERIES $VERSION $RELEASE $LATEST_SERIES $LATEST_VERSION
-        fi
-        if [ "$FLAVOR" == "focal" ] || [ "$FLAVOR" == "20.04" ] || [ "$FLAVOR" == "focal fossa"]; then
-        echo "packing focal alone"
-        ../scripts/generic_package.sh $OS $FLAVOR $SERIES $VERSION $RELEASE $LATEST_SERIES $LATEST_VERSION
-        fi
-        if [ "$FLAVOR" == "hirsute" ] || [ "$FLAVOR" == "21.04" ] || [ "$FLAVOR" == "hirsute hippo"]; then
+        ;;
+        esac
     fi
 done
 

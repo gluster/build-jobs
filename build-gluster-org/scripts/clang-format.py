885aa9c85b9d80c585b99472f19830e564f47f72
Deepshikha Khandelwal <dkhandel@redhat.com>
2021-02-16 16:48:05 +0530

    Post a comment PR on clang-format diff generation

--- build-gluster-org/scripts/clang-format.py
+++ build-gluster-org/scripts/clang-format.py
@@ -18,10 +18,15 @@ for file in changed_files:
 # Look for any changes applied by clang-format
 changed = subprocess.check_output(['git', 'diff'])
 
+#comment when there is any diff generated after running clang-format
 if changed:
+    with open('comment.txt', 'w') as file:
+    	file.write('CLANG-FORMAT FAILURE:\nBefore merging the patch, this diff needs to be considered for passing clang-format\n\n' + changed.decode("utf-8"))
     print(changed)
     print("The above patch needs to be applied to pass clang-format")
-    exit(1)
 
-# No changes, pass
-print("clang-format did not modify any files")
+else:
+    # No changes, pass
+    print("clang-format did not modify any files")
+    # workaround the ghprb plugin issue `!!! Couldn't read commit file !!!` comment when comment.txt does not exist 
+    open('comment.txt', 'a').close()

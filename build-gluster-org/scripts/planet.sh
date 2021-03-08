765b2f0b0f3eaced0a27e3438bb065521a27f452
Michael Scherer <misc@redhat.com>
2021-03-08 21:38:56 +0100

    Fix planet build
    See https://github.com/gluster/project-infrastructure/issues/118

--- build-gluster-org/scripts/planet.sh
+++ build-gluster-org/scripts/planet.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-MOCK_CHROOT=fedora-30-x86_64
+MOCK_CHROOT=fedora-32-x86_64
 
 MOCK="sudo mock -r $MOCK_CHROOT --config-opts=dnf_warning=False --config-opts=use_bootstrap_container=True"
 
@@ -8,7 +8,7 @@ $MOCK --clean
 $MOCK --init
 $MOCK --install rubygem-bundler ruby-devel curl-devel make gcc gcc-c++ ImageMagick patch zlib-devel tar git rubygem-bigdecimal
 $MOCK --copyin $WORKSPACE /src
-$MOCK --enable-network --chroot "cd /src && bundle install && bundle exec middleman build --verbose"
+$MOCK --enable-network --chroot "cd /src && bundle update --bundler && bundle install && bundle exec middleman build --verbose"
 $MOCK --copyout /src/build/ $WORKSPACE/build
 $MOCK --clean
 

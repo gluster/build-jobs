63574f5a0bc6ef981acda7032fccf80d6f15ab77
Michael Scherer <misc@redhat.com>
2021-03-10 16:38:57 +0100

    Split python2 and python3 compliance tests

--- build-gluster-org/scripts/python3-compliance.sh
+++ build-gluster-org/scripts/python3-compliance.sh
@@ -4,5 +4,4 @@ set -e
 ./configure --enable-debug --enable-gnfs --silent
 grep -rnl --exclude-dir='.git' '#!/usr/bin/python' | xargs pylint-3 --py3k
 find . -name '*.py' | xargs pylint-3 --py3k
-find . -name '*.py' | xargs python2 /opt/qa/python_compliance.py
 find . -name '*.py' | xargs python3 /opt/qa/python_compliance.py

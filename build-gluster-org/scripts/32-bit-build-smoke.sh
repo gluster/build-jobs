a3e496a269bdf664ec48e2204963d58fcdab9ae3
Deepshikha Khandelwal <dkhandel@redhat.com>
2021-03-11 17:09:46 +0530

    Add liburing-devel in the mock env

--- build-gluster-org/scripts/32-bit-build-smoke.sh
+++ build-gluster-org/scripts/32-bit-build-smoke.sh
@@ -2,7 +2,7 @@
 
 nproc=$(getconf _NPROCESSORS_ONLN)
 sudo mock -r fedora-32-i386 --config-opts=dnf_warning=False --init
-sudo mock -r fedora-32-i386 --config-opts=dnf_warning=False --install langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config clang clang-analyzer git rpcgen libtirpc-devel libuuid-devel rpcgen 
+sudo mock -r fedora-32-i386 --config-opts=dnf_warning=False --install langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config clang clang-analyzer git rpcgen libtirpc-devel libuuid-devel rpcgen liburing-devel
 sudo mock -r fedora-32-i386 --config-opts=dnf_warning=False --copyin $WORKSPACE /src
 sudo mock -r fedora-32-i386 --config-opts=dnf_warning=False --chroot "cd /src && ./autogen.sh"
 sudo mock -r fedora-32-i386 --config-opts=dnf_warning=False --chroot "cd /src && ./configure --enable-gnfs --enable-debug"

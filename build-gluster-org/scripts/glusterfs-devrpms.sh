f157a9b5760486d4b6868b2c7ee3940da030c3a7
Deepshikha Khandelwal <dkhandel@redhat.com>
2021-03-23 11:43:07 +0530

    Add the --disable-linux-io_uring flag when building glusterfs

--- build-gluster-org/scripts/glusterfs-devrpms.sh
+++ build-gluster-org/scripts/glusterfs-devrpms.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./autogen.sh || exit 1
-./configure --enable-gnfs --enable-fusermount || exit 1
+./configure --enable-gnfs --enable-fusermount --disable-linux-io_uring || exit 1
 cd extras/LinuxRPM
 make prep srcrpm || exit 1
 sudo mock -r {build_flag} --config-opts=dnf_warning=False --resultdir=${{WORKSPACE}}/RPMS/"%(dist)s"/"%(target_arch)s"/ --with=gnfs --cleanup-after --rebuild glusterfs*src.rpm || exit 1

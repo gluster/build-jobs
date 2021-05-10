581ac9a7d40cab42638041ed9ff9a0689f15e0ef
Deepshikha Khandelwal <dkhandel@redhat.com>
2021-05-10 12:18:35 +0530

    Fix the jenkins-update job

--- build-gluster-org/scripts/clang.sh
+++ build-gluster-org/scripts/clang.sh
@@ -5,7 +5,7 @@ $MOCK --init
 $MOCK --install rpcgen libtirpc-devel langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel libuuid-devel redhat-rpm-config clang clang-analyzer git
 $MOCK --copyin $WORKSPACE /src
 $MOCK --chroot "cd /src && ./autogen.sh"
-$MOCK --chroot "cd /src && ./configure CC=clang --enable-gnfs --enable-debug"
+$MOCK --chroot "cd /src && ./configure CC=clang --enable-gnfs --enable-debug --disable-linux-io_uring"
 $MOCK --chroot "cd /src && scan-build -o /src/clangScanBuildReports -disable-checker deadcode.DeadStores -v -v --use-cc clang --use-analyzer=/usr/bin/clang make"
 $MOCK --copyout /src/clangScanBuildReports $WORKSPACE/clangScanBuildReports
 $MOCK --clean
--- build-gluster-org/scripts/jenkins-update.sh
+++ build-gluster-org/scripts/jenkins-update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 virtualenv env
-env/bin/pip install -e git+https://opendev.org/jjb/jenkins-job-builder.git@775499547496777752fb606ac7121b44c6b1f2d9#egg=jenkins_jobs
+env/bin/pip install jenkins-job-builder
 env/bin/jenkins-jobs --conf $JJB_CONFIG update build-gluster-org/jobs

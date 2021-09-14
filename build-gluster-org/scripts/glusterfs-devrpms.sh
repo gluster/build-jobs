9412f9f9212123ce1987d0b4ba68243687011d3b
Rakshitha Kamath <rkamath@redhat.com>
2021-09-14 14:29:51 +0530

    Add options to enable tcmalloc for mock build Issue reference: https://github.com/gluster/project-infrastructure/issues/140

--- build-gluster-org/scripts/glusterfs-devrpms.sh
+++ build-gluster-org/scripts/glusterfs-devrpms.sh
@@ -3,4 +3,4 @@
 ./configure --enable-gnfs --enable-fusermount --disable-linux-io_uring || exit 1
 cd extras/LinuxRPM
 make prep srcrpm || exit 1
-sudo mock -r {build_flag} --config-opts=dnf_warning=False --resultdir=${{WORKSPACE}}/RPMS/"%(dist)s"/"%(target_arch)s"/ --with=gnfs --cleanup-after --rebuild glusterfs*src.rpm || exit 1
+sudo mock -r {build_flag} --config-opts=dnf_warning=False --resultdir=${{WORKSPACE}}/RPMS/"%(dist)s"/"%(target_arch)s"/ --with=gnfs --with=tcmalloc --cleanup-after --rebuild glusterfs*src.rpm || exit 1

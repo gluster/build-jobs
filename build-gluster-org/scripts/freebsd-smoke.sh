e5fda9ecec8a553c9e7f4d118bb9e0a12e1c86d6
Michael Scherer <misc@redhat.com>
2021-09-16 17:58:34 +0200

    Remove old glupy hack
    This was fixed by 871000e3ddb457c9cc5757cd94cfc178e3c1be29, not sure why
    it was still needed in 2016. But since we no longer use python2.7, I
    guess glupy work fine without it.

--- build-gluster-org/scripts/freebsd-smoke.sh
+++ build-gluster-org/scripts/freebsd-smoke.sh
@@ -1,12 +1,5 @@
 #!/usr/local/bin/bash
 
-# Added by JD on 2016/03/07 because I don't have access to edit build.sh and I'm tired
-# of these tests failing due to permissions errors in glupy.
-glupydir=/usr/local/lib/python2.7/site-packages/gluster
-sudo mkdir -p $glupydir
-sudo chown jenkins $glupydir
-sudo chmod 755 $glupydir
-
 /opt/qa/build.sh
 RET=$?
 echo $RET

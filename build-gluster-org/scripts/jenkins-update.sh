465af953599b3faf8fabf12cf2a59399088d5dd2
Michael Scherer <misc@redhat.com>
2021-12-02 16:28:27 +0100

    Allow to run JJB job on py3, to fix #111

--- build-gluster-org/scripts/jenkins-update.sh
+++ build-gluster-org/scripts/jenkins-update.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 set -e
-virtualenv env
-env/bin/pip install jenkins-job-builder
-# hack to update the certificate store
-# since that job is still running on python2, the pip installed requests module pull certifi, who has a older CA database
-# so it fail since the root CA of lets encrypt expired on 1st october 2021
-cp -f /etc/pki/tls/cert.pem  env/lib/python2.7/site-packages/certifi/cacert.pem
+virtualenv-3 env
+env/bin/pip3 install jenkins-job-builder
 env/bin/jenkins-jobs --conf $JJB_CONFIG update build-gluster-org/jobs

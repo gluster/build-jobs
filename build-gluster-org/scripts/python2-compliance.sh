3afd987478718823427d08cb79492d6aebc453cc
Michael Scherer <misc@redhat.com>
2021-12-03 17:19:12 +0100

    Remove io_uring
    Since the test is running on centos 7 (for python 2), and there
    is no io_uring, it must be disabled

--- build-gluster-org/scripts/python2-compliance.sh
+++ build-gluster-org/scripts/python2-compliance.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 set -e
 ./autogen.sh
-./configure --enable-debug --enable-gnfs --silent
+./configure --enable-debug --enable-gnfs --silent --disable-linux-io_uring
 find . -name '*.py' | xargs python2 /opt/qa/python_compliance.py

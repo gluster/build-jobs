da791f4bdbab58a002668de7bb4f4d2be3f64e07
Rakshitha Kamath <rkamath@redhat.com>
2021-12-09 11:47:20 +0530

    Providing fix to the issue - https://github.com/gluster/project-infrastructure/issues/154

--- build-gluster-org/scripts/32-bit-build-smoke.sh
+++ build-gluster-org/scripts/32-bit-build-smoke.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 nproc=$(getconf _NPROCESSORS_ONLN)
-sudo mock -r fedora-33-i386 --config-opts=dnf_warning=False --init
-sudo mock -r fedora-33-i386 --config-opts=dnf_warning=False --install langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config clang clang-analyzer git rpcgen libtirpc-devel libuuid-devel rpcgen liburing-devel gperftools-devel gperftools-libs
-sudo mock -r fedora-33-i386 --config-opts=dnf_warning=False --copyin $WORKSPACE /src
-sudo mock -r fedora-33-i386 --config-opts=dnf_warning=False --chroot "cd /src && ./autogen.sh"
-sudo mock -r fedora-33-i386 --config-opts=dnf_warning=False --chroot "cd /src && ./configure --enable-gnfs --enable-debug"
-sudo mock -r fedora-33-i386 --config-opts=dnf_warning=False --chroot "cd src && make install CFLAGS='-Wall -Werror -Wno-address-of-packed-member' -j ${nproc}"
+sudo mock -r fedora-35-i386 --config-opts=dnf_warning=False --init
+sudo mock -r fedora-35-i386 --config-opts=dnf_warning=False --install langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config clang clang-analyzer git rpcgen libtirpc-devel libuuid-devel rpcgen liburing-devel gperftools-devel gperftools-libs
+sudo mock -r fedora-35-i386 --config-opts=dnf_warning=False --copyin $WORKSPACE /src
+sudo mock -r fedora-35-i386 --config-opts=dnf_warning=False --chroot "cd /src && ./autogen.sh"
+sudo mock -r fedora-35-i386 --config-opts=dnf_warning=False --chroot "cd /src && ./configure --enable-gnfs --enable-debug"
+sudo mock -r fedora-35-i386 --config-opts=dnf_warning=False --chroot "cd /src && make install CFLAGS='-Wall -Wno-address-of-packed-member' -j ${nproc}"
 ret=$?
-sudo mock -r fedora-33-i386 --config-opts=dnf_warning=False --clean
+sudo mock -r fedora-35-i386 --config-opts=dnf_warning=False --clean
 exit $ret
--- build-gluster-org/scripts/strfmt_errors.sh
+++ build-gluster-org/scripts/strfmt_errors.sh
@@ -3,13 +3,13 @@
 ./configure --enable-fusermount || exit 1
 cd extras/LinuxRPM
 make prep srcrpm || exit 1
-sudo mock -r 'fedora-33-i386' --resultdir=${WORKSPACE}/RPMS/"%(dist)s"/"%(target_arch)s"/ --with=tcmalloc --cleanup-after --rebuild glusterfs*src.rpm || exit 1
+sudo mock -r 'fedora-35-i386' --resultdir=${WORKSPACE}/RPMS/"%(dist)s"/"%(target_arch)s"/ --with=tcmalloc --cleanup-after --rebuild glusterfs*src.rpm || exit 1
 set -x
 sudo chown -R jenkins:jenkins $WORKSPACE
 
 rm -f warnings.txt
-grep -E ".*: warning: format '%.*' expects( argument of)? type '.*', but argument .* has type 'ssize_t" ${WORKSPACE}/RPMS/fc33/i686/build.log | tee -a warnings.txt
-grep -E ".+: warning: format '%.+' expects( argument of)? type '.+', but argument .+ has type 'size_t" ${WORKSPACE}/RPMS/fc33/i686/build.log | tee -a warnings.txt
+grep -E ".*: warning: format '%.*' expects( argument of)? type '.*', but argument .* has type 'ssize_t" ${WORKSPACE}/RPMS/fc35/i686/build.log | tee -a warnings.txt
+grep -E ".+: warning: format '%.+' expects( argument of)? type '.+', but argument .+ has type 'size_t" ${WORKSPACE}/RPMS/fc35/i686/build.log | tee -a warnings.txt
 
 cat warnings.txt
 

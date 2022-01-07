7b4daaeae12b266c8569f9df8ca49966a88ba9e2
Michael Scherer <misc@redhat.com>
2022-01-07 12:16:07 +0100

    Add xxhash-devel, fix https://github.com/gluster/project-infrastructure/issues/156

--- build-gluster-org/scripts/32-bit-build-smoke.sh
+++ build-gluster-org/scripts/32-bit-build-smoke.sh
@@ -5,7 +5,7 @@ OPTIONS="-r $CHROOT --config-opts=dnf_warning=False"
 MOCK="sudo mock $OPTIONS"
 nproc=$(getconf _NPROCESSORS_ONLN)
 $MOCK --init
-$MOCK --install langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config clang clang-analyzer git rpcgen libtirpc-devel libuuid-devel rpcgen liburing-devel gperftools-devel gperftools-libs
+$MOCK --install langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel redhat-rpm-config clang clang-analyzer git rpcgen libtirpc-devel libuuid-devel rpcgen liburing-devel gperftools-devel gperftools-libs xxhash-devel
 $MOCK --copyin $WORKSPACE /src
 $MOCK --chroot "cd /src && ./autogen.sh"
 $MOCK --chroot "cd /src && ./configure --enable-gnfs --enable-debug"

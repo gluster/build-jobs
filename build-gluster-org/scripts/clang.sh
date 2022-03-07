4ee4c5f508fdc7ddfd7cfa9e429a5fcd85772d7b
Michael Scherer <misc@redhat.com>
2022-03-07 21:03:25 +0100

    Add openssl, required to build gluster

--- build-gluster-org/scripts/clang.sh
+++ build-gluster-org/scripts/clang.sh
@@ -4,7 +4,7 @@ MOCK="sudo mock -r $MOCK_CHROOT --config-opts=dnf_warning=False"
 set -e
 
 $MOCK --init
-$MOCK --install rpcgen libtirpc-devel langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel libuuid-devel redhat-rpm-config clang clang-analyzer git gperftools-devel gperftools-libs
+$MOCK --install rpcgen libtirpc-devel langpacks-en glibc-langpack-en automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel fuse-devel libuuid-devel redhat-rpm-config clang clang-analyzer git gperftools-devel gperftools-libs openssl
 $MOCK --copyin $WORKSPACE /src
 $MOCK --chroot "cd /src && ./autogen.sh"
 $MOCK --chroot "cd /src && ./configure CC=clang --enable-gnfs --enable-debug --disable-linux-io_uring"

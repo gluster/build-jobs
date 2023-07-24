891c0b70b385f25d3d2b293da2df7b3c2dae9ca7
Rakshitha Kamath <rkamath@redhat.com>
2023-07-24 13:40:13 +0530

    Update the script for jenkins-update job

--- build-gluster-org/scripts/jenkins-update.sh
+++ build-gluster-org/scripts/jenkins-update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 virtualenv-3 env
-env/bin/pip3 install jenkins-job-builder
+env/bin/pip3 install jenkins-job-builder dataclasses
 env/bin/jenkins-jobs --conf $JJB_CONFIG update build-gluster-org/jobs
